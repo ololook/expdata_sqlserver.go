1c8717ef426cddbd84e1f59d812e13202f0299ff
ololook <lfskyx@qq.com>
2016-07-11 10:15:27 +0800

    expdata_sqlserver.go
    导成sql server数据到execel

---
+++ expdata_sqlserver.go
@@ -0,0 +1,76 @@
+package main
+import (
+        "database/sql"
+        _ "github.com/denisenkom/go-mssqldb"
+        "log"
+        "github.com/tealeg/xlsx"
+        "os"
+        "fmt"
+)
+func main() {
+        arg := os.Args[1]
+        fmt.Println(arg)
+        db, err := sql.Open("mssql","server=hostip;port=port;user id=zhangyx;password=zhangyx;database=zhangyx")
+        if err != nil {
+                log.Fatalf("Open database error: %s\n", err)
+        }
+        defer db.Close()
+
+        err = db.Ping()
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        rows, err := db.Query(arg)
+        if err != nil {
+                log.Println(err)
+        }
+        defer rows.Close()
+
+        var file *xlsx.File
+        var sheet *xlsx.Sheet
+        var row *xlsx.Row
+        var cell *xlsx.Cell
+        //var err error
+
+        file = xlsx.NewFile()
+        sheet, err = file.AddSheet("Sheet1")
+        if err != nil {
+                log.Printf(err.Error())
+        } 
+        cols, _ := rows.Columns()
+        row = sheet.AddRow()
+        for i:=0;i<len(cols);i++ {
+                cell = row.AddCell()
+                cell.Value = cols[i]
+        }   
+        buff := make([]interface{}, len(cols)) 
+        data := make([]string, len(cols))  
+        for i, _ := range buff {
+             buff[i] = &data[i]  
+        }
+        for rows.Next() {
+                row = sheet.AddRow()
+                rows.Scan(buff...)
+                
+                for _, col := range data {
+                   cell = row.AddCell()
+                   cell.Value = col
+                }
+                if err != nil {
+                        log.Fatal(err)
+                }
+
+
+        }
+
+        err = rows.Err()
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        err = file.Save("test.xlsx")
+        if err != nil {
+                log.Printf(err.Error())
+        }
+}
